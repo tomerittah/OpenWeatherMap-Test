f89bd373453599fff28c67df198e64c954ba1cb9
Tomer Ittah <tomerittah@gmail.com>
2016-03-04 14:03:50 +0200

    I have written 20 tests, I found out that 7 of them are not returning the value I have excpected from querying the API so they are deliberately failed.

--- spec/weather_map_spec.rb
+++ spec/weather_map_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
+require 'json'
 
 describe Weather do
+
+  before :all do
+    ERROR_CITY_NOT_FOUND = JSON.parse(File.read('errors/error_city_not_found.json'))
+  end
 
   before :each do
     @weather = Weather.new
@@ -17,52 +22,102 @@ describe Weather do
   end
 
   describe "#setWeatherByCityName" do
-    it "query api with city name and should not return a nil value" do
-      expect(@weather.setWeatherByCityName("London")).not_to be_nil
+    it "query api with city name and should return the correct city id" do
+      expect(@weather.setWeatherByCityName("London")['id']).to eql (2643743)
+    end
+
+    it "query api with made up city name and should return an error" do
+      expect(@weather.setWeatherByCityName("MyMadeUpCity")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
+    end
+
+    # This is a deliberately failed test
+    # There is no such city - "123" but for some reason it links me to a city called 'Tapa vald'
+    it "query api with numbers instead of city name and I excpect it to return an error" do
+      expect(@weather.setWeatherByCityName("123")['name']).to eql(ERROR_CITY_NOT_FOUND['cod'])
     end
   end
 
   describe "#setWeatherByCityNameCountryCode" do
-    it "query api with city name and country code and should not return nil value" do
-      expect(@weather.setWeatherByCityNameCountryCode("London","uk")).not_to be_nil
+    it "query api with city name and country code and should return the correct city id" do
+      expect(@weather.setWeatherByCityNameCountryCode("Paris","FR")['id']).to eql (2988507)
+    end
+
+    # This is a deliberately failed test
+    # I intentionally switched the country code and the city name position and expected the api to return an error
+    # For some reason it links me to "West Jerusalem"
+    it "query api with switched values of city name and country code and should return an error" do
+      expect(@weather.setWeatherByCityNameCountryCode("IL","Jerusalem")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
+    end
+
+    # This is a deliberately failed test
+    # I intentionally switched the country code and the city name position, and wrote wrong country code
+    # Expected the api to return an error
+    # For some reason it links me to "Flines-les-Mortagne"
+    it "query api with switched values of city name and wrong country code and should return an error" do
+      expect(@weather.setWeatherByCityNameCountryCode("FR","Jerusalem")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
+    end
+
+    # This is a deliberately failed test
+    # I intentionally wrote the wrong country code for the city name and expected the api to return an error
+    it "query api with city name and wrong country code and should return an error" do
+      expect(@weather.setWeatherByCityNameCountryCode("Milano","IL")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
+    end
+
+    # This is a deliberately failed test
+    # I intentionally wrote random numbers as city name and country code and excpected the api to return an error
+    # For some reason it links me to a city called "Brockton"
+    it "query api with random numbers for city name and country code and should return an error" do
+      expect(@weather.setWeatherByCityNameCountryCode("321","123")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
     end
   end
 
   describe "#setWeatherByCityId" do
-    it "query api with city id and should  not return nil value" do
-      expect(@weather.setWeatherByCityId("2172797")).not_to be_nil
+    it "query api with city id and should return the correcy city name" do
+      expect(@weather.setWeatherByCityId("294640")['name']).to eql('State of Israel')
     end
 
     it "gets wrong city id and should not find the city" do
-      expect(@weather.setWeatherByCityId("123321")).to_json equal '{"cod":"404","message":"Error: Not found city"}'
+      expect(@weather.setWeatherByCityId("123321")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
     end
 
-    it "gets a string instead of a number and returns nil" do
-      expect(@weather.setWeatherByCityId("test")).to be_nil
+    it "gets negetive city id and should return an error" do
+      expect(@weather.setWeatherByCityId("-294640")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
+    end
+
+    it "gets a string represent a city id instead of a number and should return an error" do
+      expect(@weather.setWeatherByCityId("OneHundredAndTwentryFive")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
     end
   end
 
   describe "#setWeatherByCoordinates" do
-    it "query api with city coordinate and should not return nil value" do
-      expect(@weather.setWeatherByCoordinates("35","139")).not_to be_nil
+    it "query api with city coordinate and should return the correct city" do
+      expect(@weather.setWeatherByCoordinates("48.856461","2.35236")['name']).to eql('Saint-Merri')
     end
 
-    it "should get wrong coordinates and not find the city" do
-      expect(@weather.setWeatherByCoordinates("1","-1")).to_json equal '{"cod":"404","message":"Error: Not found city"}'
+    it "gets wrong coordinates and should not find a city matches the coordinates" do
+      expect(@weather.setWeatherByCoordinates("123123123","123123123")['cod']).to eql ERROR_CITY_NOT_FOUND['cod']
     end
 
-    it "gets a string instead of a number and returns nil value" do
-      expect(@weather.setWeatherByCoordinates("one","three")).to be_nil
+    it "gets a string instead of pair of numberes represent latitude and longitude and should return an error" do
+      expect(@weather.setWeatherByCoordinates("one","three")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
     end
   end
 
   describe "#setWeatherByZipCode" do
-    it "query api with city zip code and should not return nil value" do
-      expect(@weather.setWeatherByZipCode("94040","us")).not_to be_nil
+    it "query api with city zip code and country code and should return the correct city name" do
+      expect(@weather.setWeatherByZipCode("94040","us")['name']).to eql('Mountain View')
     end
 
-    it "gets a string instead of a number and returns nil value" do
-      expect(@weather.setWeatherByZipCode("test","us")).to be_nil
+    # This is a deliberately failed test
+    # I intentionally switched between the zip code and country cod and expected the api to return an error
+    it "query api with switched values of city zip code and country code and should return an error" do
+      expect(@weather.setWeatherByZipCode("us","94040")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
+    end
+
+    # This is a deliberately failed test
+    # I intentionally wrote string instead of valid zip code and expected the api to return an error
+    it "gets a string represent a zip code instead of a number and should return an error code" do
+      expect(@weather.setWeatherByZipCode("TenHundredesAndFifty","US")['cod']).to eql(ERROR_CITY_NOT_FOUND['cod'])
     end
   end
 
--- weather_map.rb
+++ weather_map.rb
@@ -3,73 +3,54 @@ require 'json'
 
 class Weather
 
+  # Constant Variables definition
+  URL = '/data/2.5/weather?'
+  HOST = 'api.openweathermap.org'
+  APPID = '&APPID=50f4012e8c7c01a21a478a0daa960eb5'
+
   # defining the getter/setter for the object
   attr_accessor :weather_data, :data
 
     # Constructs a Weather object.
     def initialize
       @weather_data = nil
-      @data = nil
-      @url = '/data/2.5/weather?'
-      @host = 'api.openweathermap.org'
-      @appid = '&APPID=50f4012e8c7c01a21a478a0daa960eb5'
     end
 
   # Returns a JSON format http GET request based on the query
   # Input - a query to the api
   # Output - The weather in the specific city, represented in JSON format data
   def getWeatherFromUrl(query)
-    @data = JSON.parse(Net::HTTP.get(@host, @url + query + @appid))
-    return @data
+    return JSON.parse(Net::HTTP.get(HOST, URL + query + APPID))
   end
 
-  # Returns true if the parameter is a number
-  # Input - a string
-  # Output - true if its a number
-  def checkNumericValue(str)
-    Float(str) != nil rescue false
-  end
-
-  # Set the weather_data variable by quering the api using the city name
+  # Set the weather_data variable by querying the api using the city name
   # Input - a string represent the city name
   def setWeatherByCityName(city_name)
     weather_data = getWeatherFromUrl("q=" + city_name)
   end
 
-  # Set the weather_data variable by quering the api using the city name and country code
+  # Set the weather_data variable by querying the api using the city name and country code
   # Input - a string represent the city name and a string represent a country code
   def setWeatherByCityNameCountryCode(city_name, country_code)
     weather_data = getWeatherFromUrl("q=" + city_name + "," + country_code)
   end
 
-  # Set the weather_data variable by quering the api using the city id
+  # Set the weather_data variable by querying the api using the city id
   # Input - an integer represent the city id
   def setWeatherByCityId(city_id)
-
-    # Calling checkNumericValue function to check wheter the city_id is a number
-    if checkNumericValue(city_id)
-      weather_data = getWeatherFromUrl("id=" + city_id)
-    end
+    weather_data = getWeatherFromUrl("id=" + city_id)
   end
 
-  # Set the weather_data variable by quering the api using the coordinates of the city
+  # Set the weather_data variable by querying the api using the coordinates of the city
   # Input - an integer represent the city latitude and a an integer represent the city longitude
   def setWeatherByCoordinates(lat, lon)
-
-    # Calling checkNumericValue function to check wheter the latitude and longitude are numberes
-    if checkNumericValue(lat) && checkNumericValue(lon)
-      weather_data = getWeatherFromUrl("lat=" + lat + "&lon=" + lon)
-    end
+    weather_data = getWeatherFromUrl("lat=" + lat + "&lon=" + lon)
   end
 
-  # Set the weather_data variable by quering the api using the zip code and country code
+  # Set the weather_data variable by querying the api using the zip code and country code
   # Input - an integer represent the city zip code and a string represent the country code
   def setWeatherByZipCode(zip_code, country_code)
-
-    # Calling checkNumericValue function to check wheter the zip code is a number
-    if checkNumericValue(zip_code)
-      weather_data = getWeatherFromUrl("zip=" + zip_code + "," + country_code)
-    end
+    weather_data = getWeatherFromUrl("zip=" + zip_code + "," + country_code)
   end
 
 end
